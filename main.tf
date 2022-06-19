4f7b5d195a05bc9dc622e28a7bb5d0f9420eb343
Lennart <lp@propassig.de>
2022-06-19 20:02:48 +0200

    change

--- main.tf
+++ main.tf
@@ -48,13 +48,50 @@ module "s3_bucket" {
 
 }
 
+##########
+variable iam_policy_arn {
+  description = "IAM Policy to be attached to role"
+  type = list(string)
+  default = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore", "arn:aws:iam::aws:policy/AmazonS3FullAccess"]
+}
+
+resource "aws_iam_instance_profile" "s3_ssm_profile" {
+name = "ec2_s3_ssm_profile"
+role = aws_iam_role.s3_ssm_role.name
+}
+
+
+
+resource "aws_iam_role" "s3_ssm_role" {
+name        = "ec2-s3-ssm-role"
+description = "Connect to EC2 and let EC2 write to S3"
+assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": {
+"Effect": "Allow",
+"Principal": {"Service": "ec2.amazonaws.com"},
+"Action": "sts:AssumeRole"
+}
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
+  role       = aws_iam_role.s3_ssm_role.name
+  count      = "${length(var.iam_policy_arn)}"
+  policy_arn = "${var.iam_policy_arn[count.index]}"
+}
+
+##########
+
 module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
   name = "${var.NAME}-instance"
                               
   ami                         = data.hcp_packer_image.mongodb-ubuntu.cloud_image_id // packer image
-  instance_type               = "t2.small"
+  instance_type               = "t2.micro"
   availability_zone           = element(module.vpc.azs, 0)
   monitoring                  = true
   vpc_security_group_ids      = [module.security_group.security_group_id]

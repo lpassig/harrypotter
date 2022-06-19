eee26b193fe4fff0dfbdce3cfa10722128555e4c
Lennart <lp@propassig.de>
2022-06-19 18:19:15 +0200

    typo

--- iam.tf
+++ iam.tf
@@ -29,8 +29,8 @@ EOF
 # Then parse through the list using count
 resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
   role       = aws_iam_role.s3_ssm_role.name
-  count      = "${length(var.iam-policy-arn)}"
-  policy_arn = "${var.iam-policy-arn[count.index]}"
+  count      = "${length(var.iam_policy_arn)}"
+  policy_arn = "${var.iam_policy_arn[count.index]}"
 }
 
 # resource "aws_iam_role_policy_attachment" "ssm_policy" {

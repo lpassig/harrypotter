cb072862a967b32f3c22cf6b608c3cdb98fbe9d4
Lennart <lp@propassig.de>
2022-06-19 18:11:10 +0200

    type

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 # Define policy ARNs as list
-local "iam_policy_arn" {
+locals "iam_policy_arn" {
   description = "IAM Policy to be attached to role"
   type = "list"
   default = [ "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore", "arn:aws:iam::aws:policy/AmazonS3FullAccess"]

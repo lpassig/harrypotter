411163e16d05061eb1a5a1745cf7f7f9de1c65c5
Lennart <lp@propassig.de>
2022-06-19 18:12:44 +0200

    typo

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 # Define policy ARNs as list
-locals "iam_policy_arn" {
+locals "iam-policy-arn" {
   description = "IAM Policy to be attached to role"
   type = "list"
   default = [ "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore", "arn:aws:iam::aws:policy/AmazonS3FullAccess"]
@@ -29,8 +29,8 @@ EOF
 # Then parse through the list using count
 resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
   role       = aws_iam_role.s3_ssm_role.name
-  count      = "${length(local.iam_policy_arn)}"
-  policy_arn = "${local.iam_policy_arn[count.index]}"
+  count      = "${length(local.iam-policy-arn)}"
+  policy_arn = "${local.iam-policy-arn[count.index]}"
 }
 
 # resource "aws_iam_role_policy_attachment" "ssm_policy" {
